6443bcd64e0c6aecffbf139f43e3d1a8e997fd85
BiancaIDD <pastelitoesponjosoowo@gmail.com>
2022-11-15 15:39:15 -0600

    accommodating the operating bar 🐢

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -5,14 +5,25 @@ import plus from "../../assets/plus.png";
 const Notification = () => {
   return (
     <div className="Notification__page_body">
-      <div className="Notification__add_button_bar">
-        <div className="Notification__box_text">
-          <h2 className="Notification__text_bar">Notificaciones Push</h2>
+      <div>
+        <div className="Notification__add_button_bar">
+          <div className="Notification__box_text">
+            <h2 className="Notification__text_bar">Notificaciones Push</h2>
+          </div>
+          <button className="button__new_notification">
+            <img src={plus} className="Notification__image_plus" /> Nueva
+            Notificacion
+          </button>
         </div>
-        <button className="button__new_notification">
-          <img src={plus} className="Notification__image_plus" /> Nueva
-          Notificacion
-        </button>
+      </div>
+      <div className="Notification__action_bar">
+        <p>Campaña</p>
+        <p>Inicio</p>
+        <p>Estado</p>
+        <p>Orientacion</p>
+        <p>Envios</p>
+        <p>Abiertos</p>
+        <p>% de apertura</p>
       </div>
     </div>
   );
