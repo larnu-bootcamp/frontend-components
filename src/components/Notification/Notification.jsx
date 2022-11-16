099129a1583331d48500cc2024b8f6c9235786a0
BiancaIDD <pastelitoesponjosoowo@gmail.com>
2022-11-15 21:48:31 -0600

    image set to create a new notification 🦔

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Notification.css";
 import plus from "../../assets/plus.png";
+import message from "../../assets/message.png";
+
+
+
+
 
 const Notification = () => {
+
+
   return (
     <div className="Notification__page_body">
       <div>
@@ -24,6 +31,13 @@ const Notification = () => {
         <p>Envios</p>
         <p>Abiertos</p>
         <p>% de apertura</p>
+      </div>
+      <div className="Notification__box_body">
+          <img className="Notification__image_message" src={message} />
+          <p>Crea tu primera notificacion</p>
+      </div>
+      <div>
+        
       </div>
     </div>
   );
