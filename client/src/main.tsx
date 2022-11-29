c243414153b8c511075a3e4d8a7e08ddef22e700
Jhen Nuñez <jxelektro@gmail.com>
2022-11-29 13:02:58 -0300

    index css deleted

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import LoginView from './routes/loginView';
 import DashboardView from './routes/dashboardView';
 import SignOutView from './routes/signOutView';
