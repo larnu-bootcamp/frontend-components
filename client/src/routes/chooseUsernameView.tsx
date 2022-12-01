8d29c155166c76f1f77f6fb440a851e9071a2cc5
Jhen Nuñez <jxelektro@gmail.com>
2022-11-30 22:04:43 -0300

    2Errores

--- client/src/routes/chooseUsernameView.tsx
+++ client/src/routes/chooseUsernameView.tsx
@@ -2,16 +2,15 @@ import { useEffect, useState } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import AuthProvider from "../components/authProvider";
 import { existsUsername, updateUser } from "../firebase/firebase";
-import ReactGa from 'react-ga';
+import ReactGa from "react-ga";
 import React from "react";
 
 /* Component to choose a username */
-export default function ChooseUsernameView () {
-
-  useEffect (()=>{
-    ReactGa.pageview(window.location.pathname)
+export default function ChooseUsernameView() {
+  useEffect(() => {
+    ReactGa.pageview(window.location.pathname);
   }, []);
-  
+
   const navigate = useNavigate();
   const [state, setState] = useState(0);
   const [currentUser, setCurrentUser] = useState({
@@ -24,6 +23,8 @@ export default function ChooseUsernameView () {
     providerData: [],
     providerId: "",
     uid: "",
+    username: "",
+    processCompleted: false,
   });
   const [username, setUsername] = useState(
     currentUser.displayName || currentUser.email || ""
@@ -31,71 +32,75 @@ export default function ChooseUsernameView () {
 
   /* When the user is logged in navigate to the dashboard */
   function handleUserLoggedIn(user: any) {
-    navigate('/dashboard');
+    navigate("/dashboard");
   }
 
   /*When user add a username navigate to dashboard*/
   function success() {
-    navigate('/dashboard');
+    navigate("/dashboard");
   }
 
   /* Logeado without register */
-  function handleUserNotRegistred(user: React.SetStateAction<{
-    displayName: string;
-    email: string;
-    emailVerified: boolean;
-    isAnonymous: boolean;
-    phoneNumber: string;
-    photoURL: string;
-    providerData: never[];
-    providerId: string;
-    uid: string;
-  }>) {
+  function handleUserNotRegistred(
+    user: React.SetStateAction<{
+      displayName: string;
+      email: string;
+      emailVerified: boolean;
+      isAnonymous: boolean;
+      phoneNumber: string;
+      photoURL: string;
+      providerData: never[];
+      providerId: string;
+      uid: string;
+    }>
+  ) {
     setCurrentUser(user);
     setState(3);
   }
 
   /* if user is not logged in, navigate to the login page */
   function handleUserNotLoggedIn() {
-    navigate('/login')
+    navigate("/login");
   }
 
-  /* Allow the user to choose a username */
-  function handleInputUsername(e: { target: { value: React.SetStateAction<string>; }; }) {
+  /* Function to add a username to the user with typescript*/
+  function handleInputUsername(e: React.ChangeEvent<HTMLInputElement>) {
     setUsername(e.target.value);
   }
 
+
   /*Check if the username is available */
   async function handleContinue() {
-    if (username !== '') {
+    if (username !== "") {
       const exists = await existsUsername(username);
       if (exists) {
         setState(5);
       } else {
-        const tmp = {...currentUser};
+        const tmp = { ...currentUser };
         tmp.username = username;
         tmp.processCompleted = true;
         await updateUser(tmp);
         setState(6);
       }
     }
-  } 
-  
- /* Render the choose username page to select a username*/
+  }
+
+  /* Render the choose username page to select a username*/
   if (state === 3 || state === 5) {
     return (
       <div className="chooseUserName">
         <h1>Bienvenido {currentUser.displayName || currentUser.email} </h1>
         <p>Para Terminar el proceso elige un nombre de Usuario</p>
-        {state === 5 ? <p>El nombre de usuario ya existe, escoge otro</p> : ''}
+        {state === 5 ? <p>El nombre de usuario ya existe, escoge otro</p> : ""}
         <div>
-          <input className="choooseUserName_input" type='text' onInput={handleInputUsername}>
-          </input>
+          <input
+            className="choooseUserName_input"
+            type="text"
+            onInput={handleInputUsername}
+          ></input>
         </div>
         <div>
-          <button onClick={handleContinue}>
-            Continuar
-          </button>
+          <button onClick={handleContinue}>Continuar</button>
         </div>
       </div>
     );
@@ -112,7 +117,8 @@ export default function ChooseUsernameView () {
     );
   }
   return (
-    <AuthProvider children={null}
+    <AuthProvider
+      children={null}
       onUserLoggedIn={handleUserLoggedIn}
       onUserNotRegistred={handleUserNotRegistred}
       onUserNotLoggedIn={handleUserNotLoggedIn}
